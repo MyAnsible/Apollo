a97d17eca53facad4189ac1f8ab9d4297952b9fd
enxebre <alberto.garcial@hotmail.com>
2015-05-08 14:34:20 +0100

    renaming stuff

--- bootstrap/common.sh
+++ bootstrap/common.sh
@@ -3,15 +3,16 @@
 # Util functions cloud reusable.
 
 get_ansible_requirements() {
-  ansible-galaxy install -n -r plugin-requirements.yml
+  ansible-galaxy install -f -n -r plugins.yml
 }
 
 get_apollo_ansible_variables() {
   local plugin_namespace='APOLLO_PLUGIN_'
   local var_list=()
   for i in $(env | grep ${plugin_namespace}); do
-   var=${i#${plugin_namespace}}
-   var_list+=(-var "$var")
+  	# This  Deletes shortest match of $substring from front of $string.${string#substring}
+    var=${i#${plugin_namespace}}
+    var_list+=(-var "$var")
   done
   echo ${var_list[@]}
 }
